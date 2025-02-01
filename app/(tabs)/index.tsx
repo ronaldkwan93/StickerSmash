795a2cc0d67b294b856f147b53e4f8fb02376e53
Ron Kwan <132134496+ronaldkwan93@users.noreply.github.com>
2025-02-02 08:05:35 +1100

    pick image from gallery async function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,32 @@
 import { View, StyleSheet } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
 import Button from '@/components/Button'; 
 import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
